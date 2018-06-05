5555a1b36752bd6db0af83981d3daadcfb5937e8
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-06-06 07:38:04 +0800

    Add token structure

---
+++ scanner/token.cpp
@@ -0,0 +1,11 @@
+struct Token {
+  int value;
+  string lexeme;
+  int row, column;
+  Token (int _value, string _lexeme, int _row, int _column) {
+    value  = _value;
+    lexeme = _lexeme;
+    row    = _row;
+    column = _column;
+  }
+};
