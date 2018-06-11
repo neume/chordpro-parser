9317d3adb01da5b688242990ae302ca01327f32f
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-06-11 21:36:56 +0800

    Create a wrapper class

--- main.cpp
+++ main.cpp
@@ -1,37 +1,8 @@
 #include <iostream>
-#include "scanner/scanner.cpp"
+#include "chordpro_parser.cpp"
 using namespace std;
-string get_token_string(int value) {
-  switch(value) {
-    case CHORDGROUP:       return "CHORDGROUP";
-    case LYRIC:       return "LYRIC";
-    case NEWLINE:     return "NEWLINE";
-    case DIRECTIVE:   return "DIRECTIVE";
-    case CBRACKET:    return "CBRACKET";
-    case OBRACKET:    return "OBRACKET";
-    case ADDS:        return "ADDS";
-    case CDELIMETER:  return "CDELIMETER";
-    case QUALITY:     return "QUALITY";
-    case NOTE:        return "NOTE";
-    case OBRACE:      return "OBRACE";
-    case CBRACE:      return "CBRACE";
-    case SPACE:       return "SPACE";
-    case COLON:       return "COLON";
-    case ID:          return "ID";
-    case EOF:         return "EOF";
-    case OTHER:         return "OTHER";
-    default:          return "DEFAULT";
-  }
-}
 int main() {
-  Scanner scanner = Scanner();
-  // scanner.print();
-
-  while(true) {
-    Token token = scanner.scan();
-    cout << get_token_string(token.value) << ": " << token.lexeme << endl;
-    if (token.value == EOF) {
-      break;
-    }
-  }
+  ChordproParser parser = ChordproParser();
+  parser.run();
+  return 0;
 }
