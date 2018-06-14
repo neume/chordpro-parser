a4864818eaf5bfb4342f199e219e038901ee0577
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-06-14 20:35:54 +0800

    Add constructor. Preparing chordpro_parser class

--- lib/chordpro_parser.cpp
+++ lib/chordpro_parser.cpp
@@ -1,11 +1,20 @@
 #include "scanner/scanner.cpp"
 #include "parser/parser.cpp"
+enum { CFILE, STRING };
 class ChordproParser {
+  string source;
+  int type;
 public:
+  ChordproParser() {}
+  ChordproParser(string _source, int _type=CFILE) {
+    source = _source;
+    type = _type;
+  }
   void run() {
     Scanner *scanner = new Scanner();
     Parser parser = Parser(scanner);
-    // scanner.print();
+    // Translator translator = Translator(Parser);
+
     parser.parse();
   }
 };
