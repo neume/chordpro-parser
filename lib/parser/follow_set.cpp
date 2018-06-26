28c81ec52b2ea51388056f6a01aa7d868b5bad6c
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2018-06-26 21:27:56 +0800

    Add missing set

--- lib/parser/follow_set.cpp
+++ lib/parser/follow_set.cpp
@@ -5,14 +5,15 @@ public:
       {EOF},                          // SONG
       {OBRACE, LYRIC, OBRACKET, EOF}, // FEED
       {OBRACE, LYRIC, OBRACKET, EOF}, // DIRECTIVE_GROUP
-      {ID, CBRACE},                       // KEY_VALUE
+      {ID, CBRACE},                   // KEY_VALUE
       {COLON},                        // DIRECTIVE_KEY
       {ID, CBRACE},                   // DIRECTIVE_VALUE
-      {LYRIC, OBRACKET, EOF},
-      {LYRIC, OBRACKET, EOF},
-      {CBRACKET},
-      {CBRACKET},
-      {CBRACKET}
+      {LYRIC, OBRACKET, EOF},         // LINE
+      {LYRIC, OBRACKET, EOF},         // LINE_FEED
+      {LYRIC, OBRACKET, EOF},         // CHORD_GROUP
+      {CBRACKET},                     // CHORD
+      {CBRACKET},                     // QUALITY_DECL
+      {CBRACKET}                      // ADDITIONS_DECL
     };
   }
 };
